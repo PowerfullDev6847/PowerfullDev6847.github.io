ae8ef8fd37098b2da2c194aa63c4989fa9ce2445
PowerfullDev6847 <ahmadhakimov6847@gmail.com>
2023-06-13 17:54:20 +0500

    self

---
+++ app.js
@@ -0,0 +1,16 @@
+let tg = window.Telegram.WebApp;
+
+let usercard = document.getElementById("usercard"); //получаем блок usercard 
+
+let profName = document.createElement('p'); //создаем параграф
+profName.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}
+${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
+//выдем имя, "фамилию", через тире username и код языка
+usercard.appendChild(profName); //добавляем 
+
+let userid = document.createElement('p'); //создаем еще параграф 
+userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
+usercard.appendChild(userid); //добавляе
+
+tg.ThemeParams.bg_color
